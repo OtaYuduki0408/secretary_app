bab464df44f0a0e981c1bbf25323a8cc2e210cb8
oota_yuduki <zelco054@gmail.com>
2025-10-02 10:29:09 +0900

    張とマージ

--- secretary_app/js/ChatSpace.js
+++ secretary_app/js/ChatSpace.js
@@ -38,6 +38,7 @@ export async function check_chat_Space(inputValue){
       add_calendar(inputValue)
     }else if(purpose == "R"){
       console.log("チャットの第一解析結果:カレンダー削除(R)")
+      remove_calender(inputValue)
     }else if(purpose == "G"){
       console.log("チャットの第一解析結果:カレンダー取得(G)")
     }else if(purpose == "M"){
@@ -100,6 +101,30 @@ async function add_calendar(text){
   let purpose = await gemini_request(request_text)
   console.log("第二解析(カレンダー追加処理)結果：",purpose)
   console.timeEnd("チャット解析処理、第二解析時間：カレンダー追加")
+  console.timeEnd("チャット解析処理、総所要時間")
+  alert("処理終了")
+}
+
+async function remove_calender(text) {
+  console.time("チャット解析処理、第二解析時間：カレンダー削除")
+  const now = new Date();
+  
+  let add_text = `
+  目標: カレンダー予定抽出
+  意図: 予定削除
+  抽出必須項目:
+  - name（予定名）
+  - start_time（未指定時は当日/推測時刻）
+  - end_time（未指定時は開始1H後）
+  timeはYYYY-MM-DD HH:MM:SS
+  出力はJSON配列のみ、他テキスト禁止。複数予定時はリスト化。
+  現在時刻は${now}
+  `;
+  const request_text = add_text + text
+  // 目的の関数を呼び出し
+  let purpose = await gemini_request(request_text)
+  console.log("第二解析(カレンダー削除処理)結果：",purpose)
+  console.timeEnd("チャット解析処理、第二解析時間：カレンダー削除")
   console.timeEnd("チャット解析処理、総所要時間")
   alert("処理終了")
 }
