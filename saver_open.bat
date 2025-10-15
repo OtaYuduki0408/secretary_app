4cc8cae38e1ac062bb4f116d2adcf9bcbd73fc9d
zhang48012 <206979495+zhang48012@users.noreply.github.com>
2025-10-15 10:40:02 +0900

    2を増えた

--- saver_open.bat
+++ saver_open.bat
@@ -1,10 +1,10 @@
 @echo off
 
-cd "C:\Users\y_oota\Documents\Graduation_work\secretary_app.venv\Scripts"
+cd "C:\Users\ZH_youzhi\Downloads\secretary_app\secretary_app.venv\Scripts"
 call activate.bat
-cd "C:\Users\y_oota\Documents\Graduation_work\secretary_app"
+cd "C:\Users\ZH_youzhi\Downloads\secretary_app\secretary_app"
 start /b python app.py
 
-start code "C:\Users\y_oota\Documents\Graduation_work"
+start code "C:\Users\ZH_youzhi\Downloads\secretary_app"
 
 start chrome https://127.0.0.1:5000
