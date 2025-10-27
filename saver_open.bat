e685f0bbec5126ba99d8c4eab6cd6cfdb0e29c75
1910 <i9t0@softbank.ne.jp>
2025-10-27 12:15:32 +0900

    ボタンのcss変更など 12/27/12:15

--- saver_open.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-start "Python Server" cmd /k "cd /d C:\Users\y_oota\Documents\Graduation_work\secretary_app && python -m http.server 8000"
-
-timeout /t 3 /nobreak > nul
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:8000/html
-
-code "C:\Users\y_oota\Documents\Graduation_work"
-exit
